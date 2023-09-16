641d59d6650b036797cfb295b22cfb650f137479
Ifazo <ziaulkarimifaz@outlook.com>
2023-09-17 00:27:55 +0600

    all routes almost done

--- server.ts
+++ server.ts
@@ -1,293 +1,271 @@
-import express, { Request, Response, NextFunction, query } from "express";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
-import mongoose, { Error, Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt, { Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
-import { ObjectId } from "mongodb";
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
-// middleware
+const uri = process.env.MONGODB_URI as string;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.headers.authorization;
-
   if (!authorization) {
     return res
       .status(401)
       .send({ error: true, message: "unauthorized access" });
   }
-
   const token = authorization.split(" ")[1];
   if (!token || token === "") {
     return res.status(401).send({ error: true, message: "unauthorized token" });
   }
-
+  const jwtToken = process.env.JWT_SECRET_TOKEN as string;
   try {
-    jwt.verify(token, process.env.JWT_ACCESS_TOKEN!);
+    jwt.verify(token, jwtToken);
     next();
   } catch (error) {
-    console.error(error);
     return res.status(500).send("Error verifying token");
   }
 };
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI as string)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error: Error) => console.error("MongoDB connection error:", error));
-
-// Define the Book schema
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: { type: String, required: true },
-  date: { type: String, required: true },
-  description: { type: String, required: true },
-  imgUrl: { type: String, required: true },
-});
-
-interface IBook extends Document {
-  title: string;
-  username: string;
-  email: string;
-  author: string;
-  genre: string;
-  date: string;
-  description: string;
-  imgUrl: string;
-}
-
-const Book = mongoose.model<IBook>("Book", bookSchema);
-
-// Define the User schema
-const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-interface IUser extends Document {
-  email: string;
-  password: string;
-}
-
-const User = mongoose.model<IUser>("User", userSchema);
-
-// Define the Review schema
-const reviewSchema = new Schema({
-  bookId: { type: String, required: true },
-  email: { type: String, required: true },
-  review: { type: String, required: true },
-});
-
-interface IReview extends Document {
-  bookId: string;
-  email: string;
-  review: string;
-}
-
-const Review = mongoose.model<IReview>("Review", reviewSchema);
-
-// Define the Status schema
-const statusSchema = new Schema({
-  bookId: { type: String, required: true },
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: { type: String, required: true },
-  email: { type: String, required: true },
-  status: { type: String, required: true },
-});
-
-interface IStatus extends Document {
-  bookId: string;
-  title: string;
-  author: string;
-  genre: string;
-  email: string;
-  status: string;
-}
-
-const Status = mongoose.model<IStatus>("Status", statusSchema);
-
-app.post("/user/create", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
+async function run() {
   try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.sendStatus(409);
-    }
-    const saltRounds = process.env.BCRYPT_SALT_ROUNDS || 12;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const user = new User({ email, password: hashedPassword });
+    await client.connect();
+    await client.db("book_catalog").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-    await user.save();
-    return res.sendStatus(201);
-  } catch (error) {
-    console.error(error);
-  }
-});
+    const database = client.db("book_catalog");
+    const userCollection = database.collection("users");
+    const booksCollection = database.collection("books");
+    const reviewsCollection = database.collection("reviews");
+    const statusCollection = database.collection("status");
 
-app.post("/user/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    return res.status(404);
-  }
-  if (await bcrypt.compare(password, user.password)) {
-    const payload = {
-      id: user._id,
-    };
-    const token = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN!, {
-      expiresIn: "24h",
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const JWToken = process.env.JWT_SECRET_TOKEN as Secret;
+      const token = jwt.sign(user, JWToken);
+      res.send({ token });
     });
 
-    return res.json({ token });
-  } else {
-    return res.status(403);
-  }
-});
-
-app.get("/api/books", async (req: Request, res: Response) => {
-  try {
-    const books = await Book.find();
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ message: "Error while fetching books" });
-  }
-});
-
-app.post("/api/books", async (req: Request, res: Response) => {
-  try {
-    const body = req.body;
-    const book = new Book(body);
-    await book.save();
-    res.status(201).json(book);
-  } catch (error) {
-    res.status(500).json({ message: "Error while creating book" });
-  }
-});
-
-app.get("/api/books/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const books = await Book.findOne({ _id: new ObjectId(id as string) });
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching books" });
-  }
-});
-
-app.get("/api/books/user/:email", async (req: Request, res: Response) => {
-  const { email } = req.params;
-  try {
-    const books = await Book.find({ email });
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching books" });
-  }
-});
-
-app.patch("/api/books/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const updatedBookData: IBook = req.body;
-    const updatedBook = await Book.findByIdAndUpdate(id, updatedBookData, {
-      new: true,
+    app.post("/auth/user/create", async (req: Request, res: Response) => {
+      try {
+        const data = req.body;
+        const { name, email, password, role } = data;
+        const existingUser = await userCollection.findOne({ email });
+        if (existingUser) {
+          res.send("User already exists");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = { name, email, password: hashedPassword, role };
+        console.log(user);
+        const result = await userCollection.insertOne(user);
+        res.status(200).send(result);
+      } catch (error) {
+        console.log(error);
+      }
     });
-    res.json(updatedBook);
-  } catch (error) {
-    res.status(500).json({ message: "Error updating book" });
-  }
-});
 
-app.delete("/api/books/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+    app.post("/auth/user/login", async (req: Request, res: Response) => {
+      try {
+        const { email, password } = req.body;
+        const user = await userCollection.findOne({ email });
+        if (!user) {
+          res.send("User does not exist");
+        }
+        const isPasswordCorrect = await bcrypt.compare(
+          password,
+          user?.password
+        );
+        if (!isPasswordCorrect) {
+          res.send("Password is incorrect");
+        }
+        const payload = {
+          userId: user?._id,
+          email: user?.email,
+          role: user?.role,
+        };
+        const JWTtoken = process.env.JWT_SECRET_TOKEN as Secret;
+        const token = jwt.sign(payload, JWTtoken);
+        res.status(200).send(token);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
-  try {
-    await Book.findByIdAndDelete(id);
-    res.json({ message: "Book deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting book" });
-  }
-});
-
-app.get("/api/book/reviews/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const reviews = await Review.find({ bookId: id });
-    res.json(reviews);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching reviews" });
-  }
-});
-
-app.post("/api/book/reviews", async (req: Request, res: Response) => {
-  const body = req.body;
-  try {
-    const review = new Review(body);
-    await review.save();
-    res.status(201).json(review);
-  }
-  catch (error) {
-    res.status(500).json({ message: "Error while creating review" });
-  }
-});
-
-app.get("/api/book/status/:email", async (req: Request, res: Response) => {
-  const { email } = req.params;
-  try {
-    const status = await Status.find({ email });
-    res.json(status);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching status" });
-  }
-});
-
-app.post("/api/book/status", async (req: Request, res: Response) => {
-  const body = req.body;
-  try {
-    const status = new Status(body);
-    await status.save();
-    res.status(201).json(status);
-  } catch (error) {
-    res.status(500).json({ message: "Error while creating status" });
-  }
-});
-
-app.patch("/api/book/status/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const updatedStatusData: IStatus = req.body;
-    const updatedStatus = await Status.findByIdAndUpdate(
-      id,
-      updatedStatusData,
-      {
-        new: true,
+    app.get(
+      "/api/users",
+      authMiddleware,
+      async (_req: Request, res: Response) => {
+        const users = await userCollection.find({}).toArray();
+        res.send(users);
       }
     );
-    res.json(updatedStatus);
+
+    app.get("/api/books", async (_req: Request, res: Response) => {
+      const books = await booksCollection.find({}).toArray();
+      res.send(books);
+    });
+
+    app.post(
+      "/api/books",
+      authMiddleware,
+      async (req: Request, res: Response) => {
+        try {
+          const token = req.headers.authorization?.split(" ")[1];
+          console.log(token);
+          const decodedToken = jwt.decode(token as string);
+          const { userId } = decodedToken as { userId: string };
+          const book = req.body;
+          book.userId = userId;
+          const data = { userId: userId, ...book };
+          const result = await booksCollection.insertOne(data);
+          res.status(200).send(result);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+
+    app.get("/api/books/:id", async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const book = await booksCollection.findOne({ _id: new ObjectId(id) });
+      res.send(book);
+    });
+
+    app.patch(
+      "/api/books/:id",
+      authMiddleware,
+      async (req: Request, res: Response) => {
+        try {
+          const token = req.headers.authorization?.split(" ")[1];
+          const decodedToken = jwt.decode(token as string);
+          const { userId } = decodedToken as { userId: string };
+          const { id } = req.params;
+          const findBook = await booksCollection.findOne({
+            _id: new ObjectId(id),
+          });
+          if (findBook?.userId !== userId) {
+            return res.status(401).send("Unauthorized access");
+          }
+          const book = req.body;
+          const result = await booksCollection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: book }
+          );
+          res.status(200).send(result);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+
+    app.delete(
+      "/api/books/:id",
+      authMiddleware,
+      async (req: Request, res: Response) => {
+        const token = req.headers.authorization?.split(" ")[1];
+        const decodedToken = jwt.decode(token as string);
+        const { userId } = decodedToken as { userId: string };
+        const { id } = req.params;
+        const findBook = await booksCollection.findOne({
+          _id: new ObjectId(id),
+        });
+        if (findBook?.userId !== userId) {
+          return res.status(401).send("Unauthorized access");
+        }
+        const result = await booksCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        res.send(result);
+      }
+    );
+
+    app.get("/api/reviews/:id", async (req: Request, res: Response) => {
+      const { id } = req.params;
+      const result = await reviewsCollection
+        .find({ bookId: new ObjectId(id) })
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/api/reviews/:id", authMiddleware, async (req: Request, res: Response) => {
+      const { id } = req.params;
+      const token = req.headers.authorization?.split(" ")[ 1 ];
+      const decodedToken = jwt.decode(token as string);
+      const { userId } = decodedToken as { userId: string };
+      const data = req.body;
+      data.bookId = id;
+      data.userId = userId;
+      const result = await reviewsCollection.insertOne(data);
+      console.log(result)
+      res.send(result);
+    });
+
+    app.get("/api/status", authMiddleware, async (req: Request, res: Response) => {
+      const token = req.headers.authorization?.split(" ")[ 1 ];
+      const decodedToken = jwt.decode(token as string);
+      const { email } = decodedToken as { email: string };
+      const status = await statusCollection.find({ email }).toArray();
+      res.send(status);
+    });
+
+    app.post("/api/status", authMiddleware, async (req: Request, res: Response) => {
+      const token = req.headers.authorization?.split(" ")[ 1 ];
+      const decodedToken = jwt.decode(token as string);
+      const { email } = decodedToken as { email: string };
+      const findUser = await userCollection.findOne({ email });
+      if (!findUser) {
+        return res.status(401).send("Unauthorized access");
+      }
+      const status = req.body;
+      status.email = email;
+      console.log(status)
+      const result = await statusCollection.insertOne(status);
+      res.send(result);
+    });
+
+    app.patch("/api/status/:id", authMiddleware, async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const status = req.body;
+      const result = await statusCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: status }
+      );
+      res.send(result);
+    });
+
+    app.delete("/api/status/:id", authMiddleware, async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const result = await statusCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error updating status" });
+    console.log(error);
   }
+}
+run().catch(console.dir);
+
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
 });
 
-app.get("/", async (req: Request, res: Response) => {
-  res.send("Book Catelog Server!");
-});
-
-app.get("/api", async (req: Request, res: Response) => {
-  res.send("api is running!");
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Backend app listening on port ${port}`);
 });
